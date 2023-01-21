8707e3b9203d2657cc2298ba6d5e4738ac71eb71
AlexStraubhaar <alexandre.straubhaar@gmail.com>
2023-01-21 14:04:20 +0100

    Fixed stock default value migration

--- db/migrate/20230121124418_change_default_album_stock_value.rb
+++ db/migrate/20230121124418_change_default_album_stock_value.rb
@@ -1,5 +1,5 @@
 class ChangeDefaultAlbumStockValue < ActiveRecord::Migration[7.0]
   def change
-    change_column_default :albums, :in_stock, from: nil, to: 0
+    change_column_default :albums, :in_stock, from: null, to: 0
   end
 end
